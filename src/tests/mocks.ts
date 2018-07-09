63da3cd3449b54d03225dd51572ab45e82fd54c5
Josh Madewell <joshmade@twitch.tv>
2018-07-08 23:11:07 -0500

    Convert onSuccess/onFailure to use promises

--- src/tests/mocks.ts
+++ src/tests/mocks.ts
@@ -1,105 +1,63 @@
 
 export function mockFetchForManifest() {
-  var p = new Promise((resolve, reject) => {
-    resolve({
-      ok: true,
-      json: function () {
-        return {
-          data: [
-            {
-              id: '0',
-            }
-          ]
-        }
-      }
-    });
+  return Promise.resolve({
+    ok: true,
+    json: () => ({
+      data: [{
+        id: '0',
+      }]
+    }),
   });
-  return p;
-}
-
-export function mockFetchErrorForManifest() {
-  var p = new Promise((resolve, reject) => {
-    resolve({
-      ok: false
-    });
-  });
-  return p;
 }
 
 export function mockFetchForExtensionManifest() {
-  var p = new Promise((resolve, reject) => {
-    resolve({
-      ok: true,
-      json: function () {
-        return {
-          extensions: [
-            {
-              version: 1,
-              id: 'clientid',
-              views: {}
-            }
-          ]
-        }
-      }
-    });
+  return Promise.resolve({
+    ok: true,
+    json: () => ({
+      extensions: [{
+        version: 1,
+        id: 'clientid',
+        views: {}
+      }]
+    }),
   });
-  return p;
 }
 
 export function mockFetchForUserInfo() {
-  var p = new Promise((resolve, reject) => {
-    resolve({
-      ok: true,
-      json: function () {
-        return {
-          data: [
-            {
-              login: 'test',
-              profile_image_url: 'test.png',
-            }
-          ]
-        }
-      }
-    });
+  return Promise.resolve({
+    ok: true,
+    json: () => ({
+      data: [{
+        login: 'test',
+        profile_image_url: 'test.png',
+      }]
+    })
   });
-  return p;
 }
 
 export function mockFetchError() {
-  var p = new Promise((resolve, reject) => {
-    resolve({
-      ok: false,
-    });
-  });
-  return p;
+  return Promise.resolve({ ok: false });
 }
 
 export function mockFetchProducts() {
-  const p = new Promise((resolve, reject) => {
-    resolve({
-      json: function () {
-        return {
-          products: [
-            {
-              domain: 'twitch.ext.mock',
-              sku: 'test1',
-              displayName: 'Test 1',
-              cost: {amount: 1, type: 'bits'},
-              inDevelopment: true,
-              broadcast: false
-            },
-            {
-              domain: 'twitch.ext.mock',
-              sku: 'test2',
-              displayName: 'Test 2',
-              cost: {amount: 100, type: 'bits'},
-              inDevelopment: false,
-              broadcast: false
-            }
-          ]
-        };
-      }
-    });
+  return Promise.resolve({
+    json: () => ({
+      products: [{
+        domain: 'twitch.ext.mock',
+        sku: 'test1',
+        displayName: 'Test 1',
+        cost: {amount: 1, type: 'bits'},
+        inDevelopment: true,
+        broadcast: false
+      },
+      {
+        domain: 'twitch.ext.mock',
+        sku: 'test2',
+        displayName: 'Test 2',
+        cost: {amount: 100, type: 'bits'},
+        inDevelopment: false,
+        broadcast: false
+      }]
+    })
   });
-  return p;
 }
