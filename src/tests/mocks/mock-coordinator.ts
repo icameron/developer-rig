de9a9c1d91ffeb396a0c839ba6337ea302003765
Josh Madewell <joshmade@twitch.tv>
2018-07-08 22:41:08 -0500

    Fix mocks and global types

--- src/tests/mocks/mock-coordinator.ts
+++ src/tests/mocks/mock-coordinator.ts
@@ -1,87 +1,33 @@
-export interface ExtensionMode {
-  Viewer: string;
-  Dashboard: string;
-  Config: string;
-}
+export function newMockCoordinator(): typeof window['extension-coordinator'] {
+  let coordinator = {} as typeof window['extension-coordinator'];
 
-export interface ExtensionViewType {
-  Component: string;
-  Config: string;
-  Hidden: string;
-  LiveConfig: string;
-  Mobile: string;
-  Panel: string;
-  VideoOverlay: string;
-}
-export interface ExtensionPlatform {
-  Web: string;
-  Mobile: string;
-}
-
-export interface ExtensionAnchor {
-  Panel: string;
-  Overlay: string;
-  Component: string;
-}
-
-export interface ExtensionCoordinator {
-  ExtensionMode: ExtensionMode;
-  ExtensionViewType: ExtensionViewType;
-  ExtensionPlatform: ExtensionPlatform;
-  ExtensionFrame: Function;
-  ExtensionAnchor: ExtensionAnchor;
-  getComponentPositionFromView: Function;
-  getComponentSizeFromView: Function;
-}
-
-export function newMockCoordinator(): ExtensionCoordinator {
-  let coordinator: ExtensionCoordinator;
   coordinator.ExtensionMode = {
     Viewer: 'viewer',
     Dashboard: 'dashboard',
     Config: 'config',
-  };
-
-  coordinator.ExtensionViewType = {
-    Component: 'component',
-    Config: 'config',
-    Hidden: 'hidden',
-    LiveConfig: 'liveConfig',
-    Mobile: 'mobile',
-    Panel: 'panel',
-    VideoOverlay: 'videoOverlay',
-  };
+  } as typeof coordinator.ExtensionMode;
 
   coordinator.ExtensionPlatform = {
     Web: 'web',
     Mobile: 'mobile'
-  };
-
-  coordinator.ExtensionFrame = function () {
-    return {
-      on: () => { },
-    }
-  };
+  } as typeof coordinator.ExtensionPlatform;
 
   coordinator.ExtensionAnchor = {
     Panel: 'panel',
     Overlay: 'video_overlay',
     Component: 'component',
-  };
+  } as typeof coordinator.ExtensionAnchor;
 
-  coordinator.getComponentPositionFromView = function () {
-    return {
-      x: 20,
-      y: 20,
-    }
-  }
+  coordinator.getComponentPositionFromView = () => ({
+    x: 20,
+    y: 20,
+  });
 
-  coordinator.getComponentSizeFromView = function () {
-    return {
-      width: 10,
-      height: 10,
-      zoomScale: 1024,
-    }
-  }
+  coordinator.getComponentSizeFromView = () => ({
+    width: 10,
+    height: 10,
+    zoomScale: 1024,
+  })
+
   return coordinator;
 }
